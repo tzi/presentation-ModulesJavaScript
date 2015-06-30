52c2da3b5c5896776ba7e3aa15292b026cc8898e
Thomas ZILLIOX <thomas@zilliox.me>
2015-06-30 11:22:22 +0200

    Fix 'strict mode' slide demo

--- source/javascript-scope-function-strict.js
+++ source/javascript-scope-function-strict.js
@@ -1,13 +1,14 @@
-(function(){
-  // Access global variables
-  foo = "bar";
-  // Failing assignements
-  NaN = 5;
-  // Delete undeletable properties
-  delete Object.prototype;
-  // Double property names
-  var x = {a: "1", a: "2"};
-  // Double parameter name
-  function sum (x, x) { }
-  // ...
-})
+(function () {
+    'use strict';
+    // Access global variables
+    foo = "bar";
+    // Failing assignements
+    NaN = 5;
+    // Delete undeletable properties
+    delete Object.prototype;
+    // Double property names
+    var x = {a: "1", a: "2"};
+    // Double parameter name
+    function sum(x, x) {}
+    // ...
+})();
