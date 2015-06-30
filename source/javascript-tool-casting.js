1a4ce6095429e3738532f23841ce2e2942eacd66
Thomas ZILLIOX <thomas@zilliox.me>
2015-06-30 19:10:28 +0200

    Add a slide about self-casting

---
+++ source/javascript-tool-casting.js
@@ -0,0 +1,7 @@
+console.log( true.toString() );
+console.log( (418).toString() );
+console.log( 'BrownBagLunch'.toString() );
+console.log( /Lunch$/.test('BrownBagLunch') );
+console.log( [].toString() );
+console.log( {}.toString() );
+console.log( function Module(){}.name );
