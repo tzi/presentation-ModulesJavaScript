7d0fdb1ea00eabf1e7c43db2e4e9c64fb2b6b62f
Thomas ZILLIOX <thomas@zilliox.me>
2015-06-30 11:16:05 +0200

    Add a slide on strict mode

---
+++ source/javascript-scope-function-strict.js
@@ -0,0 +1,13 @@
+(function(){
+  // Access global variables
+  foo = "bar";
+  // Failing assignements
+  NaN = 5;
+  // Delete undeletable properties
+  delete Object.prototype;
+  // Double property names
+  var x = {a: "1", a: "2"};
+  // Double parameter name
+  function sum (x, x) { }
+  // ...
+})
