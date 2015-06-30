06a9d3d14c4c9d126463b6cf5ae0f5bce4542e40
Thomas ZILLIOX <thomas@zilliox.me>
2015-06-30 19:12:24 +0200

    Add casting by default on the casting slide

--- source/javascript-tool-casting.js
+++ source/javascript-tool-casting.js
@@ -1,7 +1,7 @@
-console.log( true.toString() );
+console.log( (true).toString() );
 console.log( (418).toString() );
-console.log( 'BrownBagLunch'.toString() );
-console.log( /Lunch$/.test('BrownBagLunch') );
-console.log( [].toString() );
-console.log( {}.toString() );
-console.log( function Module(){}.name );
+console.log( ('BrownBagLunch').toString() );
+console.log( (/Lunch$/).test('BrownBagLunch') );
+console.log( ([]).toString() );
+console.log( ({}).toString() );
+console.log( (function Module(){}).name );
